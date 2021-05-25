babbf857b64efb777a1aea9d44f2c3fb544f9400
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-05-24 21:11:45 -0300

    Adding sub-tests

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,31 +6,60 @@ import (
 )
 
 type TestCase struct {
-	value int
+	value    int
 	expected bool
-	actual bool
+	actual   bool
 }
 
 func TestCalculateIsArmstrong(t *testing.T) {
-	testCase := TestCase{
-		value: 371,
-		expected: true,
-	}
+	t.Run("should return true for 371", func(t *testing.T) {
+		testCase := TestCase{
+			value:    371,
+			expected: true,
+		}
 
-	testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-	if testCase.actual != testCase.expected {
-		t.Fail()
-	}
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("should return true for 970", func(t *testing.T) {
+		testCase := TestCase{
+			value:    370,
+			expected: true,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
 }
 
 func TestNegativeCalculateIsArmstrong(t *testing.T) {
-	testCase := TestCase{
-		value: 372,
-		expected: false,
-	}
+	t.Run("should fail for case 350", func(t *testing.T) {
+		testCase := TestCase{
+			value:    350,
+			expected: false,
+		}
 
-	testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
-	if testCase.actual != testCase.expected {
-		t.Fail()
-	}
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
+	t.Run("should fail for case 300", func(t *testing.T) {
+		testCase := TestCase{
+			value:    300,
+			expected: false,
+		}
+
+		testCase.actual = calculator.CalculateIsArmstrong(testCase.value)
+		if testCase.actual != testCase.expected {
+			t.Fail()
+		}
+	})
+
 }
