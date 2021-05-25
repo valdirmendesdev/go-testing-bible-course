48363e2a40d34f36bdae5d221db98486a9792d56
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-05-24 23:20:12 -0300

    Using test data

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/valdirmendesdev/go-testing-bible-course
 
 go 1.16
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ yamltohtml/yarmltohtml_test.go
@@ -0,0 +1,43 @@
+package yamltohtml_test
+
+import (
+	"github.com/valdirmendesdev/go-testing-bible-course/yamltohtml"
+	"testing"
+
+)
+
+type TestCase struct {
+	desc     string
+	path     string
+	expected string
+}
+
+func TestYamlToHTML(t *testing.T) {
+	testCases := []TestCase{
+		TestCase{
+			desc:     "Tests title is set properly",
+			path:     "testdata/test_01.yml",
+			expected: "<html><head><title>My Awesome Page</title></head><body>This is my awesome body</body></html>",
+		},
+		TestCase{
+			desc:     "Tests body is set properly",
+			path:     "testdata/test_02.yml",
+			expected: "<html><head><title>My Second Page</title></head><body>This is my awesome body</body></html>",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			result, err := yamltohtml.YamlToHTML(test.path)
+			if err != nil {
+				t.Fail()
+			}
+
+			t.Log(result)
+
+			if result != test.expected {
+				t.Fail()
+			}
+		})
+	}
+}
